28f8c2123d99908fa0d3b61876ce88160fe3903b
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-12 21:15:27 +0500

    Init Main

--- hello.py
+++ hello.py
@@ -5,6 +5,8 @@ def greetings(name, message):
     print("Hello", name, message)
 
 name=input("Please Enter Your Name: ")
-message=input("Message: ")
+message='Welcome to this repository, Where i will play with python and try to learn it in One Day as a challange to Myself!'
 
 greetings(name, message)
+
+print('Please keep an eye out to all the folders and follow along with me as i really learn it in one day!!.')
