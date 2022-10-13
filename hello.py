a7939a877e0a5fe0a56bfb073d699bc417f1fa80
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-13 10:15:01 +0500

    Add challanges

--- hello.py
+++ hello.py
@@ -1,12 +1,10 @@
-from email import message
-
-
 def greetings(name, message):
     print("Hello", name, message)
 
-name=input("Please Enter Your Name: ")
-message='Welcome to this repository, Where i will play with python and try to learn it in One Day as a challange to Myself!'
+
+name = input("Please Enter Your Name: ")
+message = 'Welcome to this repository, Where i will play with python and try to learn it in One Day as a challange to Myself!'
 
 greetings(name, message)
 
-print('Please keep an eye out to all the folders and follow along with me as i really learn it in one day!!.')
+print('Please keep an eye out to all the folders and follow along with me as i really learn it in one day!.')
