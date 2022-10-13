449f737b091935e2070738f946a6b25ce334620c
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-13 20:57:54 +0500

    Add Fizz Buzz Challange

---
+++ challanges/fizz_buzz/fizz_buzz.py
@@ -0,0 +1,16 @@
+''' This is main Func of The Fizz buzz Challange'''
+
+def fizz_buzz(lists):
+    ''' This takes a list of Ints and returns either FIzz Or Buzz'''
+    for i in lists:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+fizz_buzz([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
+            
