c95b587c8ea83a8316588397cc2541b5e7699e44
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-14 11:52:44 +0500

    Soulution Para 1

---
+++ challanges/counting_valley/counting_valley.py
@@ -0,0 +1,18 @@
+''' Module for Counting Velly Func'''
+
+def counting_valleys(steps, path):
+    ''' Takes in a list of steps and returns an int'''
+    if int(steps) != len(path):
+        return
+    valley= 0
+    let = 0
+    for item in path:
+        if item == 'U':
+            let +=1
+        elif item == 'D':
+            let -=1
+        if let == 0:
+            valley += 1
+    result= +1 if valley % 2 == 0 else round(valley / 2)
+    print(result)
+counting_valleys(8,['U','D','D','D','U','D','U','U'])
