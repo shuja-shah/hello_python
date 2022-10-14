c3a4481b28f2fb170f75ca7869689aa1ee45f70e
shujashah83 <104627775+shujashah83@users.noreply.github.com>
2022-10-14 12:23:07 +0500

    Mix Modules

--- challanges/fizz_buzz.py
+++ challanges/fizz_buzz.py
@@ -1,6 +1,6 @@
 ''' This is main Func of The Fizz buzz Challange'''
 
-def fizz_buzz(lists):
+def fizzy(lists):
     ''' This takes a list of Ints and returns either FIzz Or Buzz'''
     for i in lists:
         if i % 3 == 0 and i % 5 == 0:
@@ -12,5 +12,4 @@ def fizz_buzz(lists):
         else:
             print(i)
 
-fizz_buzz([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20])
             
---
+++ challanges/index.py
@@ -0,0 +1,13 @@
+import angry_prof
+import counting_valley
+import fizz_buzz
+
+print('Testing Angry Professor')
+print(angry_prof.angry_professor(3, [-1, -3, 4, 2]))
+print(angry_prof.angry_professor(2, [0, -1, 2, 1]))
+
+print('Testing Counting Valley')
+print(counting_valley.counting_valleys(8, ['U','D','D','D','U','D','U','U']))
+
+print('Testing Fizz Buzz')
+print(fizz_buzz.fizzy([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]))
